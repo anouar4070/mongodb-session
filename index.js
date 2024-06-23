263405990913ba26dad02dc327ac772c90ee5747
anouar garbaa <g.anouar@yahoo.com>
2024-06-23 17:15:16 +0100

    backEnd-v1-project- started files

--- index.js
+++ index.js
@@ -3,31 +3,23 @@ const app = express()
 const port = 3000
 
 import mongoose from 'mongoose'
+import { dbConnection } from './database/dbConnection.js'
+import { userModel } from './models/user.model.js'
+import userRouter from './modules/user/user.router.js'
 
-mongoose.connect('mongodb://localhost:27017/node_anouar')
-.then(()=>{
-  console.log("database connected");
-})
-.catch((err)=> {
-  console.log("database error");
-});
 
-const userSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  age: Number
-})
+dbConnection()
 
-const userModel = mongoose.model('user', userSchema )
 
-app.get('/', async (req, res) => {
- // await userModel.insertMany({name: "Anouar", email: "anouar@gmail.com", password: "123", age: 20})
 
-   let users = await userModel.find()
-  res.json({message: "Success", users} )
-}
-)
+app.use(express.json());
+
+app.use('/v1/api/users',userRouter)
+
+
+
+
+
 
 
 // app.get('/', (req, res) => res.send('Hello World!'))
